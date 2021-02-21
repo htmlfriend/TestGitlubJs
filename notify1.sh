10860298688f70faee59c382e99a6912a0cc3297
Yurii <43734056+htmlfriend@users.noreply.github.com>
2021-02-21 17:52:35 -0500

    Add files via upload

---
+++ notify1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+TEXT = 'This notify1'
+
+ curl -s -X POST https://api.telegram.org/${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_USER_ID} -d text=$TEXT
+
+ curl -F chat_id=${TELEGRAM_USER_ID} -F document=@"public/mochawesome.html" https://api.telegram.org/${TELEGRAM_BOT_TOKEN}/sendDocument
